06e57b8f263c408a498ce20d6fac07358be49086
Zimorodek PL <henryk.kazmierczak2006@gmail.com>
2026-05-11 16:34:19 +0200

    test commit

--- Neuron.cpp
+++ Neuron.cpp
@@ -1,3 +1,3 @@
 #include "Neuron.hpp"
 
-void Neuron::activate(double input) { value = input; }
+void Neuron::activate(const double& input) { value = input; }
--- Neuron.hpp
+++ Neuron.hpp
@@ -1,7 +1,7 @@
 #pragma once
 struct Neuron {
 	double value{ 1 }, bias{};
-	void activate(double input);
+	void activate(const double& input);
 
 	Neuron(double bias = 0) : bias(bias) {};
 };
